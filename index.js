cc318491434dd917b0f516eaf973133703412a71
Eli Revah <revah@acrontum.de>
2020-05-26 09:37:24 +0200

    fix: object formatting when logging

--- index.js
+++ index.js
@@ -8,35 +8,48 @@ if (process.env.VERBOSE && trueValues.includes(process.env.VERBOSE)) {
 
 const path = require('path');
 const fs = require('fs-extra');
+const { inspect } = require('util');
+const hasAnsi = require('has-ansi');
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf, colorize } = format;
 const packageJsonPath = path.join(process.cwd(), 'package.json');
 const packageObj = fs.readJsonSync(packageJsonPath);
 const appName = packageObj.name || 'app';
+const SPLAT = Symbol.for('splat');
+
+function isPrimitive(val) {
+  return val === null || (typeof val !== 'object' && typeof val !== 'function');
+}
+function formatWithInspect(val) {
+  const prefix = isPrimitive(val) ? '' : '\n';
+  const shouldFormat = typeof val !== 'string' || !hasAnsi(val);;
+
+  return prefix + (shouldFormat ? inspect(val, { depth: null, colors: true }) : val);
+}
 
 // Custom format of the logs
 const generateItFormat = printf((info) => {
-  return `[${info.label}] ${info.timestamp} ${info.level}: ${JSON.stringify(info.message)}`;
+  const msg = formatWithInspect(info.message);
+  const splatArgs = info[SPLAT] || [];
+  const rest = splatArgs.map(data => formatWithInspect(data)).join(' ');
+
+  return `[${info.label}] ${info.timestamp} ${info.level}: ${msg} $${rest}`;
 });
 
 // Custom logging handler
 const logger = createLogger({
-  format: combine(colorize(), label({ label: appName }), timestamp(), generateItFormat),
+  format: combine(  colorize(), label({ label: appName }), timestamp(), generateItFormat),
   transports: [new transports.Console()],
 });
 
-function formatArgs(args){
-  return [util.format.apply(util.format, Array.prototype.slice.call(args))];
-}
-
 // Override the base console log with winston
-console.log = function () {
-  return logger.info.apply(logger, formatArgs(arguments));
+console.log = function (...args) {
+  return logger.info.apply(logger, [...args]);
 };
-console.error = function () {
-  return logger.error.apply(logger, formatArgs(arguments));
+console.error = function (...args) {
+  return logger.error.apply(logger, [...args]);
 };
-console.info = function () {
-  return logger.warn.apply(logger, formatArgs(arguments));
+console.info = function (...args) {
+  return logger.warn.apply(logger, [...args]);
 };
 
